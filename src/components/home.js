890178cfb2205fe4c2429d8a876e14dfc5e0d183
Erik Magnusson <erikmagnusson@Eriks-iMac.lan>
2016-03-08 23:14:32 +0100

    first state

--- src/components/home.js
+++ src/components/home.js
@@ -1,16 +1,34 @@
 import { connect } from 'react-redux';
 import React, { PropTypes, Component } from 'react';
+import actions from '../actions';
 
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
-              HOME
+                  <h1>Points: {this.props.points.currentValue}</h1>
             </div>
         );
     }
 }
 
+Home.propTypes = {
+    points: PropTypes.object.isRequired,
+};
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        points: state.points
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increase: () => {
+            dispatch(actions.pointsIncrease());
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
